661f70c2e514a2cfc3b1a9ee5be949fc01b1e863
Noah King <noahking@Noahs-MacBook-Pro.local>
2017-04-10 21:31:13 -0400

    final

--- api.js
+++
@@ -1,7 +0,0 @@
-var mapKey = "AIzaSyANiTNnqGnd6RBhUylmCbKiyNONnEipbS8";
-var breweryKey = "464c2923de039584755184680e90203c";
-    var BMCRum = {
-        startTime: new Date().getTime(),
-        token: "e14bf01a-bbe3-45c3-a6e9-fb5f9318d5fc",
-        appName: "brUSA"
-    };
